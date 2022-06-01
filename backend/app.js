4e2132f3d13e78dfc3a87c8a1c054d5785f42927
Danilo Silva <danilo.silva@imaginebeyond.com.br>
2022-06-01 12:37:52 -0300

    create api register of users

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors")
+require('dotenv').config()
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 const bodyParser = require("body-parser");
-const multipart = require("connect-multiparty")
 const multer = require("multer")
+
 const port  = process.env.PORT || 3000;
 
 const storage = multer.diskStorage({
@@ -28,6 +32,9 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
   credentials: true,
 }));
+
+// Models
+const User = require("./models/User")
 
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
@@ -61,8 +68,67 @@ app.get('/download', (req, res) => {
 
 })
 
+app.get('/', function (req, res) {
 
-app.use((err) => res.json({error: err}))
+  res.send({message: 'Bem vindo a nossa API!'})
+})
+
+
+//credencials
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+app.use(express.json())
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.w65a0yv.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+  console.log('Conectado ao banco!!')
+}).catch((err) => console.log(err))
+
+
+app.post('/auth/register', async(req, res) => {
+  const {name, email, password, confirmPassword} = req.body;
+
+  // validations
+  if(!name) {
+    return res.status(422).json({message: 'O nome é obrigatório!'})
+  }
+  if(!email) {
+    return res.status(422).json({message: 'O email é obrigatório!'})
+  }
+  if(!password) {
+    return res.status(422).json({message: 'A senha é obrigatório!'})
+  }
+
+  if(password !== confirmPassword) {
+    return res.status(422).json({message: 'As senhas não são iguais!'})
+  }
+
+  // chega se usuario existe
+  // primeira query no banco feita!!
+  const userExist = await User.findOne({email: email})
+
+  if(userExist) {
+    return res.status(422).json({message: 'Já existe uma conta com esse e-mail!'})
+  }
+
+  //cria senha
+  const salt = await bcrypt.genSalt(12)
+  const passwordHash = await bcrypt.hash(password, salt)
+
+  // cria o usuario
+  const user = new User({
+    name,
+    email,
+    password: passwordHash
+  })
+
+  try {
+    await user.save()
+    res.status(201).json({message: 'Usuario criado com sucesso!'})
+  } catch(error) {
+    res.status(500).json({message: 'Erro no servidor.. tente mais tarde!'})
+  }
+})
 
 app.listen(port, () => {
   console.log(`Servidor na porta ${port}`)
